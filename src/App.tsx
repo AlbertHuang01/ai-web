9a3a04a03737ac5b4b07beef0577b5c6553eeee9
AlbertHuang01 <37929454+AlbertHuang01@users.noreply.github.com>
2023-10-07 14:08:49 +0800

    添加 rem 配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
 
     return (
-        <h1>hello</h1>
+        <>
+            <h1>hello</h1>
+            <div className="box"></div>
+        </>
+
     )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import postcssPxtoRem from 'postcss-pxtorem'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    css: {
+        postcss: {
+            plugins: [postcssPxtoRem({
+                rootValue: 43,
+                unitPrecision: 5,
+                propList: ['*'],
+                selectorBlackList: [],
+                replace: true,
+                mediaQuery: false,
+                minPixelValue: 0,
+                exclude: /node_modules/i
+            })]
+        }
+    }
 })
