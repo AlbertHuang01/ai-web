836c7c38e3d865e703ffcea55058800c2d4138f8
AlbertHuang01 <37929454+AlbertHuang01@users.noreply.github.com>
2023-10-07 20:04:59 +0800

    实现首页列表部分

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,29 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import IconMenu from './assets/icon-menu.png'
 import IconSearch from './assets/icon-search.png'
+import IconRightArrow from './assets/icon-right-arrow.png'
+import IconEye from './assets/icon-eye.png'
+import IconStar from './assets/icon-star.png'
+
 import axios from 'axios'
 import {useEffect, useState} from "react";
-import {Carousel} from "./types.ts";
+import {Carousel, Fiction} from "./types.ts";
 
 function App() {
     const [carouselList, setCarouselList] = useState<Carousel[]>([])
+    const [fictionList, setFictionList] = useState<Fiction[]>([])
+
+    const [categoryId, setCategoryId] = useState(-1)
 
     useEffect(() => {
         axios.get<Carousel[]>('http://localhost:3000/ai/carousels').then((response) => {
             setCarouselList(response.data)
+        })
+    }, []);
+
+    useEffect(() => {
+        axios.get<Fiction[]>('http://localhost:3000/ai/fictions').then((response) => {
+            setFictionList(response.data)
         })
     }, []);
 
@@ -45,6 +58,45 @@ function App() {
                         </SwiperSlide>)}
 
                 </Swiper>
+            </div>
+            <ul className='category category-main'>
+                <li className='active'>Recommended</li>
+                <li>Fan Fiction</li>
+                <li>Sci-fi</li>
+                <li>RPG</li>
+                <li className='arrow'><img src={IconRightArrow} alt=""/></li>
+            </ul>
+            <ul className='category category-sub'>
+                <li className='active'>Default</li>
+                <li>Recent</li>
+                <li>Hot</li>
+                <li>Nodes</li>
+                <li>Filter</li>
+            </ul>
+            <div className="fiction-list">
+                {fictionList.map(item => <div className="fiction-item" key={item.id}>
+
+                    <div className="main">
+                        <img src={item.swipers[0]} alt=""/>
+                        <div className="nums">
+                            <div className="num1">
+                                <img src={IconEye} alt=""/>
+                                {item.view_num}
+                            </div>
+                            <div className="num2">
+                                <img src={IconStar} alt=""/>
+                                {item.start_num}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bottom">
+                        <div className="title">{item.title}</div>
+                        <div className="wrap">
+                            <div className="content">{item.content}</div>
+                            <img src={item.author.avatar} alt=""/>
+                        </div>
+                    </div>
+                </div>)}
             </div>
         </>
 
--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,36 @@ export interface Carousel {
     content: string
 
 }
+
+
+export interface Fiction {
+    id: number;
+
+    swipers: string[]
+
+    title: string
+
+    content: string
+
+    topics: string[]
+
+    tags: string[]
+
+    contributor_num: number
+
+    view_num: number
+
+    start_num: number
+
+    author: Author;
+}
+
+export class Author {
+    id: number;
+
+    avatar: string
+
+    name: string
+
+    fictions: Fiction []
+}
