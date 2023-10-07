539b8d4f3d93ae2a15c4ff909772f26729e7aaa0
AlbertHuang01 <37929454+AlbertHuang01@users.noreply.github.com>
2023-10-07 21:50:57 +0800

    完成详情页效果实现

---
+++ src/Detail.tsx
@@ -0,0 +1,100 @@
+import {Navigation, Pagination} from "swiper/modules";
+import {Swiper, SwiperSlide} from "swiper/react";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Carousel, Fiction} from "./types.ts";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import './Detail.scss'
+import NavLeft from './assets/nav-left.png'
+import NavRight from './assets/nav-right.png'
+import GrayCircle from './assets/gray-circle.png'
+import IconStartBlack from './assets/icon-star-black.png'
+import IconBlackArrow from './assets/icon-black-arrow.png'
+
+interface DetailProps {
+    setShowHome: (bool: boolean) => void
+}
+
+const Detail = (props: DetailProps) => {
+    const {setShowHome} = props
+    const [fiction, setFiction] = useState<Fiction>()
+
+    useEffect(() => {
+        axios.get<Fiction>('http://localhost:3000/ai/fictions/1').then((response) => {
+            setFiction(response.data)
+        })
+    }, []);
+
+    return <div className='page-wrap'>
+        <div className="nav">
+            <div className="wrap">
+                <img src={NavLeft} alt="" onClick={() => setShowHome(true)}/>
+                <img src={NavRight} alt=""/>
+            </div>
+        </div>
+        <div className="swiper">
+            <Swiper
+                navigation
+                slidesPerView={1}
+                modules={[Navigation]}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper: any) => console.log(swiper)}
+            >
+                {fiction?.swipers.map((item, index) =>
+                    <SwiperSlide key={index}>
+                        <img src={item} alt=""/>
+                    </SwiperSlide>)}
+
+            </Swiper>
+        </div>
+
+        <div className="title">{fiction?.title}</div>
+
+        <div className="author">
+            <img src={fiction?.author.avatar} alt=""/>
+            {fiction?.author.name}
+        </div>
+
+        <p className='content'>{fiction?.content}</p>
+
+        <div className="topics">
+            {fiction?.topics.map((item, index) => <span key={index}>#{item}</span>)}
+        </div>
+        <div className="topics">
+            {fiction?.topics.map((item, index) => <span key={index}>#{item}</span>)}
+        </div>
+
+        <div className="tags">
+            {fiction?.tags.map((item, index) => <span key={index}>{item}</span>)}
+        </div>
+
+        <div className="fiction-data">
+            <div className="group">
+                <div className="circle-group">
+                    <img src={GrayCircle} alt=""/>
+                    <img src={GrayCircle} alt=""/>
+                    <img src={GrayCircle} alt=""/>
+                </div>
+                <div className="contributors">+{fiction?.contributor_num} contributors</div>
+            </div>
+            <div className="star">
+                <img src={IconStartBlack} alt=""/>
+                {fiction?.start_num}
+            </div>
+
+
+        </div>
+        <div className="enter">
+            <div className="wrap">
+                Enter Dream
+            </div>
+        </div>
+        <div className='view-detail'>
+            View Dream Details
+            <img src={IconBlackArrow} alt=""/>
+        </div>
+    </div>
+}
+
+export default Detail
---
+++ src/Home.tsx
@@ -0,0 +1,120 @@
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Pagination} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import IconMenu from './assets/icon-menu.png'
+import IconSearch from './assets/icon-search.png'
+import IconRightArrow from './assets/icon-right-arrow.png'
+import IconEye from './assets/icon-eye.png'
+import IconStar from './assets/icon-star.png'
+import IconBottom1 from './assets/icon-bottom-1.png'
+import IconBottom2 from './assets/icon-bottom-2.png'
+import IconBottom3 from './assets/icon-bottom-3.png'
+import {useEffect, useState} from "react";
+import {Carousel, Fiction} from "./types.ts";
+import axios from "axios";
+
+import './Home.scss'
+
+const Home = () => {
+    const [carouselList, setCarouselList] = useState<Carousel[]>([])
+    const [fictionList, setFictionList] = useState<Fiction[]>([])
+
+    const [categoryId, setCategoryId] = useState(-1)
+
+    useEffect(() => {
+        axios.get<Carousel[]>('http://localhost:3000/ai/carousels').then((response) => {
+            setCarouselList(response.data)
+        })
+    }, []);
+
+    useEffect(() => {
+        axios.get<Fiction[]>('http://localhost:3000/ai/fictions').then((response) => {
+            setFictionList(response.data)
+        })
+    }, []);
+
+    return <>
+        <img src={IconMenu} alt="" className='menu'/>
+        <div className="search">
+            <img src={IconSearch} alt=""/>
+            <input type="text" placeholder='Search for dreams'/>
+        </div>
+        <div className="home-swiper">
+            <Swiper
+                pagination
+                modules={[Pagination]}
+                slidesPerView={1}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                {carouselList.map(item =>
+                    <SwiperSlide key={item.id}
+                                 style={{backgroundImage: `url(${item.img_url})`}}>
+                        <div className="text">
+                            <div className="text-wrap">
+                                <div className='title'>{item.title}</div>
+                                <div className='content'>{item.content}</div>
+                                <div className='btn'>Get started</div>
+                            </div>
+                        </div>
+                    </SwiperSlide>)}
+
+            </Swiper>
+        </div>
+        <ul className='category category-main'>
+            <li className='active'>Recommended</li>
+            <li>Fan Fiction</li>
+            <li>Sci-fi</li>
+            <li>RPG</li>
+            <li className='arrow'><img src={IconRightArrow} alt=""/></li>
+        </ul>
+        <ul className='category category-sub'>
+            <li className='active'>Default</li>
+            <li>Recent</li>
+            <li>Hot</li>
+            <li>Nodes</li>
+            <li>Filter</li>
+        </ul>
+        <div className="fiction-list">
+            {fictionList.map(item => <div className="fiction-item" key={item.id}>
+
+                <div className="main">
+                    <img src={item.swipers[0]} alt=""/>
+                    <div className="nums">
+                        <div className="num1">
+                            <img src={IconEye} alt=""/>
+                            {item.view_num}
+                        </div>
+                        <div className="num2">
+                            <img src={IconStar} alt=""/>
+                            {item.start_num}
+                        </div>
+                    </div>
+                </div>
+                <div className="bottom">
+                    <div className="title">{item.title}</div>
+                    <div className="wrap">
+                        <div className="content">{item.content}</div>
+                        <img src={item.author.avatar} alt=""/>
+                    </div>
+                </div>
+            </div>)}
+        </div>
+        <div className="home-bottom">
+            <div className="wrap">
+                <div className="home">
+                    <img src={IconBottom1} alt=""/>
+                    <span>Home</span>
+                </div>
+                <img src={IconBottom3} alt=""/>
+                <div className="mine">
+                    <img src={IconBottom2} alt=""/>
+                    <span>Mine</span>
+                </div>
+            </div>
+        </div>
+    </>
+}
+
+export default Home
