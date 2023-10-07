d582096143776d2aa54bc3904b51d563d46c0f4a
AlbertHuang01 <37929454+AlbertHuang01@users.noreply.github.com>
2023-10-07 17:57:17 +0800

    实现首页轮播图部分

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,51 @@
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Pagination} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import IconMenu from './assets/icon-menu.png'
+import IconSearch from './assets/icon-search.png'
+import axios from 'axios'
+import {useEffect, useState} from "react";
+import {Carousel} from "./types.ts";
+
 function App() {
+    const [carouselList, setCarouselList] = useState<Carousel[]>([])
+
+    useEffect(() => {
+        axios.get<Carousel[]>('http://localhost:3000/ai/carousels').then((response) => {
+            setCarouselList(response.data)
+        })
+    }, []);
 
     return (
         <>
-            <h1>hello</h1>
-            <div className="box"></div>
+            <img src={IconMenu} alt="" className='menu'/>
+            <div className="search">
+                <img src={IconSearch} alt=""/>
+                <input type="text" placeholder='Search for dreams'/>
+            </div>
+            <div className="home-swiper">
+                <Swiper
+                    pagination
+                    modules={[Pagination]}
+                    slidesPerView={1}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => console.log(swiper)}
+                >
+                    {carouselList.map(item =>
+                        <SwiperSlide key={item.id}
+                                     style={{backgroundImage: `url(${item.img_url})`}}>
+                            <div className="text">
+                                <div className="text-wrap">
+                                    <div className='title'>{item.title}</div>
+                                    <div className='content'>{item.content}</div>
+                                    <div className='btn'>Get started</div>
+                                </div>
+                            </div>
+                        </SwiperSlide>)}
+
+                </Swiper>
+            </div>
         </>
 
     )
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface Carousel {
+    id: number;
+
+    img_url: string;
+
+    title: string
+
+    content: string
+
+}
